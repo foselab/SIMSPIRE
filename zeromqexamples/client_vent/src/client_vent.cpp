6eda60632af2978476e885269687cfb35e52ea73
Angelo Gargantini <angelo.gargantini@unibg.it>
2021-11-02 18:59:50 +0100

    client in C++

---
+++ zeromqexamples/client_vent/src/client_vent.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : client_vent.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include <string>
+#include <iostream>
+
+#include <zmq.hpp>
+
+int main()
+{
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
+
+    // construct a REQ (request) socket and connect to interface
+    zmq::socket_t socket{context, zmq::socket_type::req};
+    socket.connect("tcp://localhost:5555");
+
+    // set up some static data to send
+    const std::string data{"Hello"};
+
+    for (auto request_num = 0; request_num < 10; ++request_num)
+    {
+        // send the request message
+        std::cout << "Sending Hello " << request_num << "..." << std::endl;
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+
+        // wait for reply from server
+        zmq::message_t reply{};
+        socket.recv(reply, zmq::recv_flags::none);
+
+        std::cout << "Received " << reply.to_string();
+        std::cout << " (" << request_num << ")";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
