841e2cb68874b680ddb9e4b04c2c4ca587bdcb50
Cleo98 <s.piazzi@studenti.unibg.it>
2022-06-21 23:17:39 +0200

    Add zmq logic for ventilator side

--- zeromq_schema_ventilation/server_vent/src/server_vent/Ventilator.java
+++ zeromq_schema_ventilation/server_vent/src/server_vent/Ventilator.java
@@ -1,6 +1,7 @@
 package server_vent;
 
 import org.zeromq.ZMQ;
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 
 /**
@@ -11,18 +12,23 @@ public class Ventilator {
 
 	public static void main(String[] args) throws Exception {
 		try (ZContext context = new ZContext()) {
-			// Socket to talk to clients
-			ZMQ.Socket socket = context.createSocket(ZMQ.REP);
+			/*
+			 * A socket of type REP is used by a service to receive requests from a client and 
+			 * send replies to a client. It allows only an alternating sequence of recv(request) and
+			 * subsequent send(reply) calls.
+			 */
+			ZMQ.Socket socket = context.createSocket(SocketType.REP);
 			socket.bind("tcp://localhost:5555");
-			System.out.println("starting ventilator");
+			System.out.println("starting ventilator...");
+			
 			while (!Thread.currentThread().isInterrupted()) {				
-				// do not block if nothing i requested
-				byte[] reply = socket.recv(org.zeromq.ZMQ.DONTWAIT);
+				// If there are no messages available on the specified socket, reply is null (non-blocking mode)
+				byte[] reply = socket.recv(ZMQ.DONTWAIT);
 
 				if (reply != null) {					
 					// Print the message
 					String msg = new String(reply, ZMQ.CHARSET);
-					System.out.println("Received: [" + msg + "]");
+					System.out.println("Received message: [" + msg + "]");
 
 					//	se mi domanda la pressione, restituisco il valore
 					if (msg.equals("getPressure")) {
@@ -33,9 +39,7 @@ public class Ventilator {
 						// Send a response
 						socket.send(response.getBytes(ZMQ.CHARSET), 0);
 					}
-				}
-				// update il ventilatore o altro
-				// TODO				
+				}			
 			}
 		}	
 	}
